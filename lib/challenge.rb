3a6803b2d2f425210b885dbb3a3cd380b505f99d
Antony Donovan <antony.donovan@generalassemb.ly>
2017-01-05 09:50:56 -0500

    Add solution
    One commit on 015/master.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 def calc(expr)
   # Your code here
+  stack = []
+  expr.split(' ').each do |token|
+    if token =~ /\d+/
+      stack << token.to_f
+    else
+      stack << token.to_sym.to_proc.call(stack.slice!(-2), stack.slice!(-1))
+    end
+  end
+  stack.last || 0
 end
