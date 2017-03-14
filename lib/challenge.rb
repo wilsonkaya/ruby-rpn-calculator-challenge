3f1a8a35e850a22ce0981ba6ddba16329d5f5ece
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-14 14:17:32 -0400

    Create Reverse Polish Notation

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 def calc(expr)
-  # Your code here
+  num = expr.split(' ')
+  num0 = num[0].to_i
+  num1 = num[1].to_i
+  num2 = num[2].to_i
+
+  def result(num1, num2, operator)
+    num
+  end
+
+  result(num0, num1, num2)
 end
+
+calc("2 3 +")
